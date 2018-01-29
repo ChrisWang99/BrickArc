2a0e83188c15fae3db5d4dd638aa6c88c9df5a2d
Unknown <1084714805@qq.com>
2018-01-29 18:51:46 +0800

    加入死亡检测

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    public Collider2D MapArea;
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!rb.IsTouching(MapArea)) {
+            Debug.Log("Player Died!");
+        }
+	}
+}
